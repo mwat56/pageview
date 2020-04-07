c143accbf27b2b66a1effbf899322512809f3789
Matthias Watermann <support@mwat.de>
2020-04-07 23:23:43 +0200

    * updated file;

--- pageview_test.go
+++ pageview_test.go
@@ -45,6 +45,7 @@ func TestCreateImage(t *testing.T) {
 	u1 := "http://dev.mwat.de/"
 	n1 := sanitise(u1) + `.` + wkImageFileType
 	u2 := "http://www.mwat.de/"
+	n2 := "httpwwwmwatde.png"
 	u3 := "http://www.mwat.de/index.pl"
 	n3 := sanitise(u3) + `.` + wkImageFileType
 	u4 := "http://bla.mwat.de/index.shtml"
@@ -61,7 +62,7 @@ func TestCreateImage(t *testing.T) {
 	}{
 		// TODO: Add test cases.
 		{" 1", args{u1}, n1, false},
-		{" 2", args{u2}, "", true},
+		{" 2", args{u2}, n2, false},
 		{" 3", args{u3}, n3, false},
 		{" 4", args{u4}, n4, false},
 	}
